ed13bc341dac313d61510eb9495217e6b740400a
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-04 22:53:06 -0500

    listening to  port

--- app/server.js
+++ app/server.js
@@ -1,9 +1,17 @@
 //Incorporate dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 //call express
+var app = express();
 // Declare a port
+var PORT = 3000;
 
 
 
 
 
 //Listen to port
+app.listen(PORT, ()=>{
+    console.log('listening to PORT: '+ PORT);
+});
