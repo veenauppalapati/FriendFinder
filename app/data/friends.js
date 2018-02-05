a9caa18c95ede13ab7f504b9ffd41408de7d4707
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-04 23:42:30 -0500

    friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,22 @@
 // array of objects
+var friends = [
+    {
+        name:"Ahmed",
+        photourl:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        questions:[
+                    5,
+                    1,
+                    4,
+                    4,
+                    5,
+                    1,
+                    2,
+                    5,
+                    4,
+                    1
+        ]
+    }
+
+]
+
+module.exports = {friends: friends}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,36 @@
 //ROUTING
+//Incorporate dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var friendsModule = require('../data/friends.js');
+var friends = friendsModule.friends;
+console.log(friends);
+//call express
+var app = express();
+// Declare a port
+var PORT = 3000;
+
+//data parsing
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 // A GET Route to `/survey` which should display the survey page.
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname+"/../public/", "survey.html"));
+});
 // A default, catch-all route that leads to `home.html` which displays the home page. 
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname+"/../public/", "home.html"));
+});
+
+app.post("/survey", function(req, res){
+    var newPerson = req.body;
+    newPerson.routeName = newPerson.name.replace(/\s+/g, "").toLowerCase();
+    // console.log(newPerson);
+    friends.push(newPerson);
+})
+//Listen to port
+app.listen(PORT, ()=>{
+    console.log('listening to PORT: '+ PORT);
+});
