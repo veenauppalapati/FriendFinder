beb5b2b3df72331dbe8d2382fd49df9a8424e8ca
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-07 20:38:22 -0500

    math formula for  accessing second Arrays elements

--- app/data/math.js
+++ app/data/math.js
@@ -5,13 +5,28 @@ var b = [ [1, 4, 5], [2, 3, 5], [4, 3, 1]];
 // second Array has array of multiple elements questions array.  arrays inside of an array
 function findAMatch (firstArray, secondArray) {
     //iterate through array b 
-    // compare each index's elements with array a elements
-    // find the difference for each element's index 
-    // push the difference to a results array
+    // b.forEach(element => {
+        
+    // });
+    // 
+    // compare each index's elements with array firstArray elements
+    var tempArray = [];
+
+    for (var i = 0; i< secondArray.length; i++){
+        tempArray.push(firstArray[i]   -   secondArray[0][i]);
+    }
+    console.log(tempArray);
+    // b.forEach((element, index)=> {
+    //     console.log(element[element[index]]);
+    // // find the difference for each element's index 
+    
+    // // push the difference to a results array
+    // });
+   
     // add the elements in the results array
     // push the added results into a compatiblity array
     // look for the lowest number in the compatibility array
     // based on the number from the compatibility array, use it as an index to find the obect in friends array
 
 }
-findAMatch();
+findAMatch(a,b);
