8156d4b9000576cb2770fafde27268039f95a0df
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-07 20:07:46 -0500

    math.js pseudocode

---
+++ app/data/math.js
@@ -0,0 +1,17 @@
+var a = [ 2, 3, 5];
+var b = [ [1, 4, 5], [2, 3, 5], [4, 3, 1]];
+
+// first Array has one element questions
+// second Array has array of multiple elements questions array.  arrays inside of an array
+function findAMatch (firstArray, secondArray) {
+    //iterate through array b 
+    // compare each index's elements with array a elements
+    // find the difference for each element's index 
+    // push the difference to a results array
+    // add the elements in the results array
+    // push the added results into a compatiblity array
+    // look for the lowest number in the compatibility array
+    // based on the number from the compatibility array, use it as an index to find the obect in friends array
+
+}
+findAMatch();
